eeb98dd6986b4e27e673201a3b775a204562a936
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-04-15 22:57:19 +0200

    GH #760 - Preserved install.sh for backward compatibility.

--- code/install.sh
+++ code/install.sh
@@ -1,1 +1,9 @@
-scripts/install.sh
+#!/bin/bash
+
+CURDIR="${BASH_SOURCE[0]}";RL="readlink";([[ `uname -s`=='Darwin' ]] || RL="$RL -f")
+while([ -h "${CURDIR}" ]) do CURDIR=`$RL "${CURDIR}"`; done
+N="/dev/null";pushd .>$N;cd `dirname ${CURDIR}`>$N;CURDIR=`pwd`;popd>$N
+
+# This script is preserved for 2.0-compatiblity only and will be removed in the future.
+# Use make install directly instead of it.
+make -f $CURDIR/Makefile
