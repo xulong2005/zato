6603ed0ed73b8d9151a66f6e62c7e845ec30e3f9
Laurent Bercot <ska-devel@skarnet.org>
2017-06-05 13:51:32 +0200

    GH #762 alpine: enforce both py- and py2- packages for numpy and numpy-f2py, both are needed

--- code/_install-alpine.sh
+++ code/_install-alpine.sh
@@ -30,7 +30,7 @@ fi
 
   sudo apk update
 
-  sudo apk add openblas py-numpy py-numpy-f2py --update-cache --repository "$PREFERRED_REPOSITORY/$ALPINE_FLAVOUR/community"
+  sudo apk add openblas py-numpy py-numpy-f2py py2-numpy py2-numpy-f2py --update-cache --repository "$PREFERRED_REPOSITORY/$ALPINE_FLAVOUR/community"
   sudo apk add py-scipy --update-cache --repository "$PREFERRED_REPOSITORY/edge/testing"
   sudo apk add gcc g++ git gfortran haproxy libbz2 libev libev-dev libevent libevent-dev \
     libgfortran libffi-dev libldap libpq libsasl libuuid libxml2-dev libxslt-dev \
